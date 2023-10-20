eced562fcdc9d852fd19deae1c65ae06b5f8fe0b
ARITRA BAG <57993082+riktiger@users.noreply.github.com>
2023-10-21 04:38:01 +0530

    Files for improved IPL Match Winner Predictor

--- ipl_winner_predictor_ui.py
+++ ipl_winner_predictor_ui.py
@@ -12,7 +12,7 @@ import pickle
 import streamlit as st
 
 #teams_dataset = pd.read_csv('/home/aritrabag/Documents/internship_projects/teams.csv')
-#venue_dataset = pd.read_csv('/home/aritrabag/Documents/internship_projects/venues.csv')
+venue_dataset = pd.read_csv('/home/aritrabag/Documents/internship_projects/venues.csv')
 
 #team_list = teams_dataset['batting_team'].unique()
 #print(team_list)
@@ -20,7 +20,28 @@ teams = ['Royal Challengers Bangalore', 'Delhi Capitals','Mumbai Indians', 'King
 
 #venue_list = venue_dataset['venue'].unique()
 #print(venue_list)
-venues = ['Rajiv Gandhi International Stadium, Uppal , Hyderabad',  'M Chinnaswamy Stadium , Bangalore', 'Wankhede Stadium , Mumbai', 'Holkar Cricket Stadium , Indore', 'Eden Gardens , Kolkata',  'Feroz Shah Kotla , Delhi',  'Punjab Cricket Association IS Bindra Stadium, Mohali , Chandigarh', 'Punjab Cricket Association Stadium, Mohali , Chandigarh',  'Sawai Mansingh Stadium , Jaipur', 'MA Chidambaram Stadium, Chepauk , Chennai', 'Dr DY Patil Sports Academy , Mumbai', 'Newlands , Cape Town',  "St George's Park , Port Elizabeth" ,'Kingsmead , Durban' , 'SuperSport Park , Centurion', 'Buffalo Park , East London',  'New Wanderers Stadium , Johannesburg', 'De Beers Diamond Oval , Kimberley','OUTsurance Oval , Bloemfontein', 'Brabourne Stadium , Mumbai', 'Sardar Patel Stadium, Motera , Ahmedabad' , 'Barabati Stadium , Cuttack',  'Vidarbha Cricket Association Stadium, Jamtha , Nagpur' , 'Himachal Pradesh Cricket Association Stadium , Dharamsala',  'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium , Visakhapatnam',  'Subrata Roy Sahara Stadium , Pune',  'Shaheed Veer Narayan Singh International Stadium , Raipur', 'JSCA International Stadium Complex , Ranchi',  'Sheikh Zayed Stadium , Abu Dhabi' ,'Sharjah Cricket Stadium , Sharjah', 'Maharashtra Cricket Association Stadium , Pune',  'Punjab Cricket Association IS Bindra Stadium, Mohali , Mohali', 'M Chinnaswamy Stadium , Bengaluru', 'M. A. Chidambaram Stadium , Chennai', 'Feroz Shah Kotla Ground , Delhi' , 'M. Chinnaswamy Stadium , Bengaluru',  'Rajiv Gandhi Intl. Cricket Stadium , Hyderabad',  'IS Bindra Stadium , Mohali' ,'ACA-VDCA Stadium , Visakhapatnam']
+venues = ['Rajiv Gandhi International Stadium, Uppal , Hyderabad' ,
+ 'M. Chinnaswamy Stadium , Bengaluru', 'Wankhede Stadium , Mumbai',
+ 'Holkar Cricket Stadium , Indore', 'Eden Gardens , Kolkata',
+ 'Feroz Shah Kotla , Delhi',
+ 'Punjab Cricket Association IS Bindra Stadium, Mohali , Chandigarh',
+ 'Sawai Mansingh Stadium , Jaipur',
+ 'MA Chidambaram Stadium, Chepauk , Chennai',
+ 'Dr DY Patil Sports Academy , Mumbai', 'Newlands , Cape Town',
+ "St George's Park , Port Elizabeth", 'Kingsmead , Durban',
+ 'SuperSport Park , Centurion', 'Buffalo Park , East London',
+ 'New Wanderers Stadium , Johannesburg',
+ 'De Beers Diamond Oval , Kimberley', 'OUTsurance Oval , Bloemfontein',
+ 'Brabourne Stadium , Mumbai', 'Sardar Patel Stadium, Motera , Ahmedabad',
+ 'Barabati Stadium , Cuttack',
+ 'Vidarbha Cricket Association Stadium, Jamtha , Nagpur',
+ 'Himachal Pradesh Cricket Association Stadium , Dharamsala',
+ 'Dr. Y.S. Rajasekhara Reddy ACA-VDCA Cricket Stadium , Visakhapatnam',
+ 'Subrata Roy Sahara Stadium , Pune',
+ 'Shaheed Veer Narayan Singh International Stadium , Raipur',
+ 'JSCA International Stadium Complex , Ranchi',
+ 'Sheikh Zayed Stadium , Abu Dhabi' 'Sharjah Cricket Stadium , Sharjah',
+ 'Maharashtra Cricket Association Stadium , Pune']
 
 predictor = pickle.load(open('/home/aritrabag/Documents/internship_projects/predictor.pkl','rb'))
 st.title('IPL Dream Team')
@@ -34,16 +55,16 @@ with col2:
 
 venue = st.selectbox('Venue',sorted(venues))
 
-target = st.number_input('Target')
+target = st.number_input('Target',step = 1)
 
 col3,col4,col5 = st.columns(3)
 
 with col3:
-    runs = st.number_input(' Current Score')
+    runs = st.number_input(label =' Current Score', step = 1)
 with col4:
-    overs = st.number_input('Overs completed')
+    overs = st.number_input(label = 'Overs completed', min_value = 0.0, max_value = 20.0, step = 0.1)
 with col5:
-    wickets = st.number_input('Wickets Lost')
+    wickets = st.number_input(label = 'Wickets Lost', min_value = 0, max_value = 10, step = 1)
 
 if st.button('Who Will Win the Match ?'):
     runs_left = target - runs
